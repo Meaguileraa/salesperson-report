55421dc52abe6eb295e84a9bf8d78bbd4770627f
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-16 21:05:51 -0700

    First functionwith dict

--- sales_report.py
+++ sales_report.py
@@ -1,25 +1,18 @@
-"""Generate sales report showing total melons each salesperson sold."""
+def who_sold_what(file):
 
+    """Generate sales report showing total melons each salesperson sold."""
 
-salespeople = []
-melons_sold = []
+    report_dict = {}
 
-f = open('sales-report.txt')
-for line in f:
-    line = line.rstrip()
-    entries = line.split('|')
+    with open(file) as f:
+        for line in f:
+            line = line.rstrip()
+            name, total, melons_sold = line.split('|')
 
-    salesperson = entries[0]
-    melons = int(entries[2])
+            if name in report_dict:
+                report_dict[name] += int(melons_sold)
+            else:
+                report_dict[name] = int(melons_sold)
+    return report_dict
+who_sold_what('sales-report.txt')
 
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
-
-        melons_sold[position] += melons
-    else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
-
-
-for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
